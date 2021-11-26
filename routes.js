f8373a012a81f10ff13eec7899ceec07fa468bf1
Gabriel-Malenowitch <gabrielbotelhomalenowitch@gmail.com>
2021-11-25 22:41:28 -0400

    alpha2

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 export const paths = [
     {
         '360p' : {
-            'src': '../videos/360p.mp4',
+            'src': './videos/360p.mp4',
             'type': 'video/mp4'
         },
         '720p' : {
-            'src': '../videos/720p.mp4',
+            'src': './videos/720p.mp4',
             'type': 'video/mp4'
         },
     }
